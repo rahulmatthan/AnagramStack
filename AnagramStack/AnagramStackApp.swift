00c5671510ff8fc5420a4b8a88f5b7d541639012
rahulmatthan <matthan@gmail.com>
2026-02-12 12:56:07 +0530

    Initial Commit

---
+++ AnagramStack/AnagramStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnagramStackApp.swift
+//  AnagramStack
+//
+//  Created by Rahul Matthan on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AnagramStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnagramStackTests/AnagramStackTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnagramStackTests.swift
+//  AnagramStackTests
+//
+//  Created by Rahul Matthan on 12/02/26.
+//
+
+import Testing
+
+struct AnagramStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
